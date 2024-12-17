1c8dbd4ceddcf0e09daa7fdd8772b61d375a4ae8
r4mercur <bjarnmb9@gmail.com>
2024-12-17 17:23:17 +0100

    fix update of fields

--- schema.py
+++ schema.py
@@ -7,11 +7,32 @@ from dotenv import load_dotenv
 
 def update_solr_schema(temp_solr_url: str, temp_collection_name: str, temp_schema: dict) -> None:
     schema_url = f'{temp_solr_url}/{temp_collection_name}/schema'
-    response = requests.post(schema_url, json=temp_schema)
+    response = requests.get(schema_url)
+
+    if response.status_code != 200:
+        print(f'Failed to retrieve current schema: {response.text}')
+        return
+
+    current_schema = response.json()
+    temp_current_schema = current_schema.get('schema', {})
+
+    schema_version = temp_current_schema.get('version')
+    existing_fields = {field['name'] for field in temp_current_schema.get('fields', [])}
+
+    new_fields = [field for field in temp_schema.get('add-field', [])
+                  if field['name'] not in existing_fields]
+
+    if not new_fields:
+        print(f'No new fields to add, to schema with version: {schema_version}')
+        return
+
+    update_payload = {'add-field': new_fields}
+    response = requests.post(schema_url, json=update_payload)
     if response.status_code == 200:
         print('Schema updated successfully.')
     else:
         print(f'Failed to update schema: {response.text}')
+
 
 def reload_solr_collection(temp_solr_url: str, temp_collection_name: str) -> None:
     reload_url = f'{temp_solr_url}/admin/collections?action=RELOAD&name={temp_collection_name}'
@@ -20,19 +41,6 @@ def reload_solr_collection(temp_solr_url: str, temp_collection_name: str) -> Non
         print(f'Collection {temp_collection_name} reloaded successfully.')
     else:
         print(f'Failed to reload collection {temp_collection_name}: {response.text}')
-
-
-def add_field_to_existing_schema(temp_solr: str, temp_collection: str, new_field: str) -> None:
-    schema_url = f'{temp_solr}/{temp_collection}/schema'
-    response = requests.get(schema_url)
-    temp_schema = response.json()
-    temp_schema['fields'].append(json.loads(new_field))
-
-    response = requests.post(schema_url, json=temp_schema)
-    if response.status_code == 200:
-        print('Field added successfully.')
-    else:
-        print(f'Failed to add field: {response.text}')
 
 
 if __name__ == '__main__':
